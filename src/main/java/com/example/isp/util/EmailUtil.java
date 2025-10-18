26835b5cb3767734fde33f804d79c74e8abed8b9
Binguyxn <quocuonguyen12345@gmail.com>
2025-10-18 11:16:17 +0700

    fix Sendgrid & Render

--- src/main/java/com/example/isp/util/EmailUtil.java
+++ src/main/java/com/example/isp/util/EmailUtil.java
@@ -1,29 +1,52 @@
 package com.example.isp.util;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Random;
 
 @Component
-@RequiredArgsConstructor
 public class EmailUtil {
 
-    private final JavaMailSender mailSender;
+    @Value("${SENDGRID_API_KEY}")
+    private String sendgridApiKey;
 
+    // Sinh mã OTP ngẫu nhiên 6 chữ số
     public String generateOtp() {
         return String.format("%06d", new Random().nextInt(999999));
     }
 
-    public void sendVerificationEmail(String to, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("Xác thực tài khoản");
-        message.setText("Mã OTP của bạn là: " + otp + "\nMã có hiệu lực trong 5 phút.");
-        message.setFrom("nhacnhoviet1@gmail.com");
-        mailSender.send(message);
-        System.out.println("Đã gửi OTP tới: " + to);
+    // Gửi email xác thực qua SendGrid API
+    public void sendVerificationEmail(String to, String otp) throws IOException {
+        Email from = new Email("nhacnhoviet1@gmail.com");
+        String subject = "Xác thực tài khoản Nhắc Nhỏ Việt";
+        Email toEmail = new Email(to);
+        Content content = new Content("text/plain",
+                "Xin chào,\n\nMã OTP xác thực tài khoản của bạn là: " + otp +
+                        "\nMã có hiệu lực trong 5 phút.\n\nTrân trọng,\nNhóm phát triển Nhắc Nhỏ Việt");
+        Mail mail = new Mail(from, subject, toEmail, content);
+
+        SendGrid sg = new SendGrid(sendgridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+
+            System.out.println("Email sent to " + to + " | Status Code: " + response.getStatusCode());
+        } catch (IOException ex) {
+            System.err.println("Failed to send email to " + to + ": " + ex.getMessage());
+            throw ex;
+        }
     }
 }
