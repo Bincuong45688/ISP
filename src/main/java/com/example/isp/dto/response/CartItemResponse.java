72611ac62232b6e574ef4559560a58f8d074f023
To Ngoc Anh Tuan <anhtuan120605@gmail.com>
2025-10-21 11:02:15 +0700

    hiển thị đầy đủ trong card

--- src/main/java/com/example/isp/dto/response/CartItemResponse.java
+++ src/main/java/com/example/isp/dto/response/CartItemResponse.java
@@ -1,16 +1,19 @@
 package com.example.isp.dto.response;
 
 import lombok.*;
+import java.math.BigDecimal;
 
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class CartItemResponse {
-
+    private Long cartItemId;
     private Long productId;
     private String productName;
-    private Integer quantity;     
-    private Boolean selected;     // Trạng thái được chọn (true/false)
+    private String productImage;
+    private BigDecimal unitPrice;
+    private int quantity;
+    private BigDecimal lineTotal;
+    private boolean selected;
 }
