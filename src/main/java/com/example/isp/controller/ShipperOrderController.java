82d668e36a7525697bf396164fc94dbe1d8b7e9e
To Ngoc Anh Tuan <anhtuan120605@gmail.com>
2025-11-05 20:26:24 +0700

    upload anh cho shipper

--- src/main/java/com/example/isp/controller/ShipperOrderController.java
+++ src/main/java/com/example/isp/controller/ShipperOrderController.java
@@ -1,12 +1,13 @@
 package com.example.isp.controller;
 
-
 import com.example.isp.dto.response.OrderResponse;
 import com.example.isp.service.ShipperService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -20,30 +21,30 @@ public class ShipperOrderController {
     @PutMapping("/{orderId}/accept")
     public ResponseEntity<?> acceptOrder(@PathVariable Long orderId) {
         shipperService.acceptOrder(orderId);
-        return  ResponseEntity.ok("Order accepted successfully. Status: SHIPPING");
+        return ResponseEntity.ok("Order accepted successfully. Status: SHIPPING");
     }
 
-    // Cập nhật trạng thái hoàn tất đơn SHIPPING -> COMPLETED)
-    @PutMapping("/{orderId}/complete")
-    public ResponseEntity<?> completeOrder(@PathVariable Long orderId) {
+    // SHIPPING -> COMPLETED (+ upload POD)
+    @PutMapping(value = "/{orderId}/complete", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> completeOrder(
+            @PathVariable Long orderId,
+            @RequestPart("proofImage") MultipartFile proofImage
+    ) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        shipperService.completeOrder(orderId, username);
+        shipperService.completeOrder(orderId, username, proofImage);
         return ResponseEntity.ok("Order marked as complete successfully");
     }
 
-    // Lấy các đơn đang chờ shipper xác nhận
     @GetMapping("/pending")
     public ResponseEntity<List<OrderResponse>> getPendingOrders() {
         return ResponseEntity.ok(shipperService.getPendingOrders());
     }
 
-    // Lấy các đơn đang giao
     @GetMapping("/active")
     public ResponseEntity<List<OrderResponse>> getActiveOrders() {
         return ResponseEntity.ok(shipperService.getActiveOrders());
     }
 
-    // Lấy các đơn đã hoàn thành
     @GetMapping("/completed")
     public ResponseEntity<List<OrderResponse>> getCompletedOrders() {
         return ResponseEntity.ok(shipperService.getCompletedOrders());
